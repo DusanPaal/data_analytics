186ffbf1db590d7e6884fe5793c5cd60ac7a71d8
Dusan Paal <dusanpaal@gmail.com>
2024-10-03 20:43:44 +0200

    update

--- bitcoin.py
+++ bitcoin.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import stock_analysis
 
+from stock_analysis.utils import group_stocks, describe_group, make_portfolio
+
 # read data from file
 data = pd.read_csv(
   r'data/bitcoin/bitcoin.csv',
@@ -8,12 +10,40 @@ data = pd.read_csv(
   parse_dates=True
 )
 
+reader = stock_analysis.StockReader('2019-01-01', '2020-12-31')
 
+fb, apple, amazon, netflix, google = (
+  reader.get_ticker_data(ticker)
+  for ticker in ['FB', 'AAPL', 'AMZN', 'NFLX', 'GOOGL']
+)
 
+sp = reader.get_index_data('S&P 500')
+bitcoin = reader.get_bitcoin_data('USD')
 
+# what does involve story telling in data analysis?
 
+faang = group_stocks({
+  'Facebook': fb, 'Apple': apple,
+  'Amazon': amazon, 'Netflix': netflix,
+  'Google': google
+})
 
+faang_sp = group_stocks({
+  'Facebook': fb, 'Apple': apple, 'Amazon': amazon,
+  'Netflix': netflix, 'Google': google, 'S&P 500': sp
+})
 
+all_assets = group_stocks({
+  'Bitcoin': bitcoin, 'S&P 500': sp, 'Facebook': fb,
+  'Apple': apple, 'Amazon': amazon, 'Netflix': netflix,
+  'Google': google
+})
+
+desc = describe_group(all_assets)
+print(desc)
+
+portfolio = make_portfolio(all_assets)
+print(portfolio)
 
 breakpoint()
 pass
---
+++ stock_analysis/utils.py
@@ -0,0 +1,168 @@
+"""Utility functions for stock analysis."""
+
+from functools import wraps
+import re
+
+import pandas as pd
+
+def _sanitize_label(label: str) -> str:
+  """Clean up a label by removing non-letter, non-space
+  characters and putting in all lowercase with underscores
+  replacing spaces.
+
+  Parameters:
+  -----------
+  label: str
+    The text to be fixed.
+
+  Returns:
+  --------
+  str
+  The sanitized label.
+  """
+
+  # remove leading and trailing empty spaces
+  label = label.lower().strip()
+
+  # replace any duplicate spaces in  
+  # the label text with a single space
+  label = re.sub(r'\s+', ' ', label)
+
+  # remove non-letter and non-space characters
+  label = re.sub(r'[^\W]', '', label)
+
+  # replace spaces with underscores
+  label = label.replace(' ', '_')
+
+  return label
+
+def label_sanitizer(method):
+  """Decorator around a method that returns a dataframe 
+  to clean up all labels in said dataframe (column names 
+  and index name) by using `_sanitize_label()`.
+  
+  Parameters:
+  -----------
+  method: 
+    The method to wrap.
+
+  Returns:
+  --------
+  A decorated method or function.
+  """
+
+  @wraps(method) # keeps the original docstrig for help()
+  def method_wrapper(self, *args, **kwargs):
+    """Wrap the method to sanitize the labels."""
+
+    # call the original method
+    df = method(self, *args, **kwargs)
+
+    # fix the column names
+    df.columns = [_sanitize_label(col) for col in df.columns]
+
+    # fix the index name
+    df.index.name = _sanitize_label(df.index.name)
+
+    return df
+  
+  return method_wrapper
+
+def group_stocks(mapping: dict) -> pd.DataFrame:
+  """Create a new dataframe with many assets and a new 
+  column indicating the asset that row's data belongs to.
+
+  Parameters:
+  -----------
+  mapping: 
+  A key-value mapping of the form {asset_name: asset_df}
+
+  Returns:
+  --------
+  A new `pandas.DataFrame` object.
+  """
+
+  groups = []
+
+  for stock, stock_data in mapping.items():
+    df = stock_data.copy(deep=True).assign(name=stock)
+    groups.append(df)
+
+  groups_df = pd.concat(groups, sort=True)
+  groups_df.index = pd.to_datetime(groups_df.index)
+
+  return groups_df
+
+def validate_dataframe(columns, instance_method=True):
+  """Decorator that raises a `ValueError` if input isn't
+  a `DataFrame` or doesn't contain the proper columns. Note
+  the `DataFrame` must be the first positional argument
+  passed to this method.
+
+  Parameters:
+  -----------
+  columns: 
+    A set of required column names.
+    For example, {'open', 'high', 'low', 'close'}.
+
+  instance_method: 
+    Whether or not the item being decorated
+    is an instance method. Pass `False to
+    decorate static methods and functions.
+
+  Returns:
+  --------
+  A decorated method or function.
+  """
+
+  def method_wrapper(method):
+    """Wrap the method to sanitize the labels."""
+
+    @wraps(method)
+    def validate_wrapper(self, *args, **kwargs):
+      # functions and static methods don't pass self so
+      # self is the 1st positional argument in that case
+      df = (self, *args)[0 if not instance_method else 1]
+
+      if not isinstance(df, pd.DataFrame):
+        raise ValueError('The first argument must be a pandas.DataFrame!')
+
+      if columns.difference(df.columns):
+        raise ValueError(
+          f'Dataframe must contain the following columns: {columns}'
+        )
+
+      return method(self, *args, **kwargs)
+
+    return validate_wrapper
+  
+  return method_wrapper
+
+@validate_dataframe(columns={'name'}, instance_method=False)
+def describe_group(data: pd.DataFrame) -> pd.DataFrame:
+  """Run `describe()` on the asset group.
+
+  Parameters:
+  -----------
+  data:
+  Grouped data resulting from `group_stocks()`
+
+  Returns:
+  --------
+  The transpose of the grouped description statistics.
+  """
+  return data.groupby('name').describe().T
+
+@validate_dataframe(columns=set(), instance_method=False)
+def make_portfolio(data, date_level='date'):
+  """Make a portfolio of assets by grouping 
+  by date and summing all columns.
+
+  Note: 
+  -----
+  The caller is responsible for making sure the
+  dates line up across assets and handling when they don't.
+  """
+  return data.groupby(date_level).sum()
+
+
